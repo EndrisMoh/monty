b976573939ec3e416e24274bf7c32251e8299ecb
EndrisMoh <endrismd@gmail.com>
2022-10-06 12:31:11 -0700

    Implement the pall opcode

--- 0-pall.c
+++ 0-pall.c
@@ -1,26 +1,21 @@
 #include "monty.h"
 
 /**
- * m_pall - print all values on `stack' starting from the top
- * @stack: double pointer to head of stack
- * @line_number: line number being executed from script file
+ * pall - prints the data of all nodes in stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
  *
- * Return: void
+ * Return: Nothing.
  */
-void m_pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int nline)
 {
-	stack_t *head;
+	stack_t *temp;
+	(void)nline;
 
-	(void)(line_number);
-
-	head = *stack;
-	while (head != NULL)
+	temp = *stack;
+	while (temp)
 	{
-		printf("%d\n", head->n);
-		head = head->next;
-		if (head == *stack)
-		{
-			return;
-		}
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
